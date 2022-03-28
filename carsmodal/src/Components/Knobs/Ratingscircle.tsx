35daa4bac3bdb8f33726e9cf4262b4c635560f7d
deepahuja <deepahuja@deloitte.com>
2022-03-28 20:25:02 +0530

    Add Knobs Chart

--- carsmodal/src/Components/Knobs/Ratingscircle.tsx
+++ carsmodal/src/Components/Knobs/Ratingscircle.tsx
@@ -1,26 +1,117 @@
-import { useState } from "react";
-import { Col, Container, Row } from "react-bootstrap";
+import { Col, Row } from "react-bootstrap";
 import { Donut } from "react-dial-knob";
 
 const Ratingscircle = () => {
-    const [value,setValue]=useState(0);
   return (
     <>
       <Row className="border-bottom mb-2">
-        <Col>
+        <Col
+          className="col-2 alignmentCenter"
+          style={{ width: "135px !important;" }}
+        >
           <Donut
             diameter={50}
             min={0}
             max={5}
-            step={0.1}
-            value={value}
+            step={1}
+            value={2}
             theme={{
-              donutColor: "blue",
+              donutColor: "#6f6f6f",
             }}
-            onValueChange={setValue}
             ariaLabelledBy={"my-label"}
+            spaceMaxFromZero={false}
           >
-            <label id={"my-label"}>Some label</label>
+            <label id={"my-label"} className="donut-label">
+              Exterior
+            </label>
+          </Donut>
+        </Col>
+        <hr className="spacehrrating"></hr>
+        <Col
+          className="col-2 alignmentCenter"
+          style={{ width: "135px !important;" }}
+        >
+          <Donut
+            diameter={50}
+            min={0}
+            max={5}
+            step={1}
+            value={2}
+            theme={{
+              donutColor: "#6f6f6f",
+            }}
+            ariaLabelledBy={"Comfort"}
+            spaceMaxFromZero={false}
+          >
+            <label id={"Comfort"} className="donut-label">
+              Comfort
+            </label>
+          </Donut>
+        </Col>
+        <hr className="spacehrrating"></hr>
+        <Col
+          className="col-2 alignmentCenter"
+          style={{ width: "135px !important;" }}
+        >
+          <Donut
+            diameter={50}
+            min={0}
+            max={5}
+            step={1}
+            value={2}
+            theme={{
+              donutColor: "#6f6f6f",
+            }}
+            ariaLabelledBy={"Performance"}
+            spaceMaxFromZero={false}
+          >
+            <label id={"Performance"} className="donut-label">
+              Performance
+            </label>
+          </Donut>
+        </Col>
+        <hr className="spacehrrating"></hr>
+        <Col
+          className="col-2 alignmentCenter "
+          style={{ width: "135px !important;" }}
+        >
+          <Donut
+            diameter={50}
+            min={0}
+            max={5}
+            step={1}
+            value={2}
+            theme={{
+              donutColor: "#6f6f6f",
+            }}
+            ariaLabelledBy={"Fuel-Economy"}
+            spaceMaxFromZero={false}
+          >
+            <label id={"Fuel-Economy"} className="donut-label">
+              Fuel Economy
+            </label>
+          </Donut>
+        </Col>
+        <hr className="spacehrrating"></hr>
+        <Col
+          className="col-2 alignmentCenter"
+          style={{ width: "135px !important;" }}
+        >
+          <Donut
+            diameter={50}
+            min={0}
+            max={5}
+            step={1}
+            value={2}
+            theme={{
+              donutColor: "#6f6f6f",
+            }}
+            ariaLabelledBy={"Value-For-Money"}
+            spaceMaxFromZero={false}
+          >
+            <label id={"Value-For-Money"} className="donut-label">
+              Value For Money
+            </label>
           </Donut>
         </Col>
       </Row>
