70d05d7930a93a90f25eb21a21a85a284d7d681c
deepahuja <deepahuja@deloitte.com>
2022-03-29 19:03:09 +0530

    e2

--- carsmodal/src/App.tsx
+++ carsmodal/src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
 import "./App.css";
 import Accordian from "./Components/Accordian/Accordian";
 import Header from "./Components/Header/Header";
